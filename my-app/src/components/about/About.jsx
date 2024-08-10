6720c14b4aef7347ce230dda7aa5b3bbfe019b34
wowitsselina <168394330+wowitsselina@users.noreply.github.com>
2024-08-09 22:55:02 -0700

    Update About.jsx
    explain what kendo is and separating it from mission

--- my-app/src/components/about/About.jsx
+++ my-app/src/components/about/About.jsx
@@ -27,10 +27,12 @@ class AboutPage extends Component {
           <div className='mission-statement about-text'>
             <h3 className ='mission-statement-title'>What is Kendo?</h3>
             <p className = 'mission-statement-sub'>Kendo is a Japanese fencing-like martial art which uses 
-              bamboo swords (shinai) and derives from the fighting style of samurai. The Kendo Club at UCI 
-              provides an environment for Kendokas (practitioners of Kendo) to train and build a community 
-              with their fellow Kenshi. We welcome people of all skill levels and encourage all to come and 
-              train with us.</p>
+              bamboo swords (shinai) and derives from the fighting style of samurai.
+              </p>
+            <h3 className ='mission-statement-title'>Why us?</h3>
+            <p className ='mission-statement-sub'>The Kendo Club at UCI 
+              is committed to providing a safe and welcoming environment for all students, regardless of
+              experience or fitness level to learn Kendo. We welcome all to come and train with us.</p>
           </div>
 
           <div className='sensei-info sensei-about-text'>
