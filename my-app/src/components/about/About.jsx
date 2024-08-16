f8ef8ebaabba08541d000ad1c8713e2ab00cdb14
wowitsselina <168394330+wowitsselina@users.noreply.github.com>
2024-08-15 23:23:37 -0700

    update about

--- my-app/src/components/about/About.jsx
+++ my-app/src/components/about/About.jsx
@@ -19,18 +19,7 @@ class AboutPage extends Component {
         <div className='about'>
           <Navbar/>
           <div className='about-title about-text'>
-            <Title text='About Us'/>
-          </div>
-
-          <div className='mission-statement about-text'>
-            <h3 className ='mission-statement-title'>What is Kendo?</h3>
-            <p className = 'mission-statement-sub'>Kendo is a Japanese fencing-like martial art which uses 
-              bamboo swords (shinai) and derives from the fighting style of samurai.
-              </p>
-            <h3 className ='mission-statement-title'>Why us?</h3>
-            <p className ='mission-statement-sub'>The Kendo Club at UCI 
-              is committed to providing a safe and welcoming environment for all students, regardless of
-              experience or fitness level to learn Kendo. We welcome all to come and train with us.</p>
+            <Title text='Leadership'/>
           </div>
 
           <div className='sensei-info sensei-about-text'>
