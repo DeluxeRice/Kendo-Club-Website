527cffc44c69ba5daf3a7b87fb47d0b548f95120
wowitsselina <168394330+wowitsselina@users.noreply.github.com>
2024-09-19 16:25:08 -0700

    update schedule

--- my-app/src/containers/schedule/Schedule.jsx
+++ my-app/src/containers/schedule/Schedule.jsx
@@ -11,22 +11,22 @@ const Schedule = () => {
                     </div>
                     <div className='kendo__schedule-text-sub'>
                         <div className='kendo__schedule-text-time'>
-                            <p>Mondays</p>
+                            <p>Mondays @ Training Zone</p>
                             <p>7:30pm-9:00pm</p>
                         </div>
                     </div>
-                    <p>@ ARC Physical Forum</p>
                     <div className='kendo__schedule-text-sub'>
                         <div className='kendo__schedule-text-time'>
-                            <p>Fridays</p>
-                            <p>7:00-8:50pm</p>
+                            <p>Fridays @ Training Zone</p>
+                            <p>7-9pm, 9:30-11pm</p>
                         </div>
+                    <div className='kendo__schedule-text-sub'></div>
                         <div className='kendo__schedule-text-time'>
-                            <p>Saturday</p>
+                            <p>Saturday @ Physical Forum</p>
                             <p>12pm-2pm</p>
                         </div>
-                    </div>
-                    <a href='https://drive.google.com/file/d/1tuAdz7URQwfIluH8JbRX8-ZFMaYcgr1w/view' className='kendo__schedule-text-location' target='_blank' rel='noreferrer'>@ ARC Training Zone</a>
+                        </div>
+                    <a href='https://drive.google.com/file/d/1tuAdz7URQwfIluH8JbRX8-ZFMaYcgr1w/view' className='kendo__schedule-text-location' target='_blank' rel='noreferrer'>CLick for Directions</a>
                 </div>
             </div>
         </div>
